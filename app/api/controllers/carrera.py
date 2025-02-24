21c63a0e11e99da105430d123a37429f0d1b8780
jaris-10 <perez.jaris10@gmail.com>
2025-02-24 07:54:41 -0500

    crud carreras

--- app/api/controllers/carrera.py
+++ app/api/controllers/carrera.py
@@ -1,7 +1,8 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.api.schemes.carrera import CarreraCrear, CarreraResponse
+from app.api.schemes import carrera
+from app.api.schemes.carrera import CarreraCrear, CarreraEditar, CarreraResponse
 from app.database.db import get_db_session
 from app.database.models.carrera import Carrera
 
@@ -26,8 +27,19 @@ def obtener_item(id: int,db: Session = Depends(get_db_session)):
     carrera = db.query(Carrera).filter_by(id_carrera=id).first()
     return carrera
 
+#Patch /{id_carrera}
+@router.patch("/{id}", response_model=CarreraResponse)
+def editar_item(id: int, carrera:CarreraEditar,db: Session = Depends(get_db_session)):
+    db_carrera = db.query(Carrera).filter_by(id_carrera=id).first()
+    db_carrera.nombre = carrera.nombre
+    db.commit()
+    db.refresh(db_carrera)
+    return db_carrera
     
-
-#PATCH /{id_carrera}
-
 #DElETE /{id_carrera}
+@router.delete("/{id}")
+def eliminar_item(id: int,db: Session = Depends(get_db_session)):
+    db_carrera = db.query(Carrera).filter_by(id_carrera=id).first()
+    db.delete(db_carrera)
+    db.commit()
+    return {"message":"Carrera eliminada"}
