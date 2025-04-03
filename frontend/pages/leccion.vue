c9d5342d053d92aef44f71ede46d1952c781384b
Oscar Pérez <operez@customsoft.biz>
2025-04-03 10:23:48 -0500

    actualizacion frontend

---
+++ frontend/pages/leccion.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hola {{ nombre }}</h1>
+    <h3>Contador: {{ contador }}</h3>
+    <Button 
+        @click="contador++" 
+        icon="pi pi-plus" 
+        aria-label="Save" 
+    />
+    <Button 
+        @click="contador--" 
+        icon="pi pi-minus" 
+        aria-label="Save" 
+    />
+    
+</template>
+
+<script setup lang="ts">
+const nombre = ref<string>('Pepito')
+const contador = ref<number>(0)
+
+</script>
---
+++ frontend/pages/materias2.vue
@@ -0,0 +1,18 @@
+<template>
+    <DataTable :value="materias" tableStyle="min-width: 50rem">
+        <Column field="code" header="Nombre"></Column>
+        <Column field="name" header="Creditos"></Column>
+        <Column field="category" header="Tipo"></Column>
+    </DataTable>
+</template>
+<script setup lang="ts">
+import { Materia } from '~/features/materias/models/materia-model';
+
+    const materias=ref<Materia[]>([]);
+    onMounted(async() => {
+        const response = await fetch('http://localhost:3200/api/v1/materias');
+        const data = await response.json();
+        materias.value = data;
+    });
+
+</script>
