0b5ab8d33926383fb561a0141d0931b13cf4c128
Oscar Pérez <operez@customsoft.biz>
2025-04-07 07:40:03 -0500

    crear una carrera

--- frontend/pages/materias2.vue
+++ frontend/pages/materias2.vue
@@ -1,18 +1,20 @@
 <template>
     <DataTable :value="materias" tableStyle="min-width: 50rem">
-        <Column field="code" header="Nombre"></Column>
-        <Column field="name" header="Creditos"></Column>
-        <Column field="category" header="Tipo"></Column>
+        <Column field="nombre" header="Nombre"></Column>
+        <Column field="creditos" header="Creditos"></Column>
+        <Column field="tipo" header="Tipo"></Column>
     </DataTable>
 </template>
 <script setup lang="ts">
-import { Materia } from '~/features/materias/models/materia-model';
+import type { MateriaModel } from '~/features/materias/models/materia-model';
 
-    const materias=ref<Materia[]>([]);
+
+    const materias=ref<MateriaModel[]>([]);
     onMounted(async() => {
         const response = await fetch('http://localhost:3200/api/v1/materias');
         const data = await response.json();
         materias.value = data;
+        console.log(data);
     });
 
 </script>
