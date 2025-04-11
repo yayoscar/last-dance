468fcae7412ff7c835391eddcdadfb3bd4b16dcb
Oscar Pérez <operez@customsoft.biz>
2025-04-11 09:42:52 -0500

    plan_estudio_materias

--- app/api/schemes/plan_estudio.py
+++ app/api/schemes/plan_estudio.py
@@ -17,3 +17,25 @@ class PlanResponse(PlanBase):
     
 class PlanEditar(PlanBase):
     id_plan: int
+
+class MateriaAsignacion(BaseModel):
+    id_materia: int
+    semestre: int = Field(gt=0, lt=13, description="Semestre entre 1 y 12")
+
+class PlanEstudioMateriaAgregar(BaseModel):
+    materias: List[MateriaAsignacion]
+
+class MateriaPlanResponse(BaseModel):
+    id_materia: int
+    nombre: str
+    creditos: int
+    tipo: str
+    id_modulo: int
+    semestre: int  # Viene de la tabla intermedia
+
+class PlanEstudioConMateriasResponse(BaseModel):
+    id_plan_estudio: int
+    nombre: str
+    materias: List[MateriaPlanResponse]
+
+    model_config = ConfigDict(from_attributes=True)
